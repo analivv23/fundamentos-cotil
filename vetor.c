70a17c473b947d48cecfbf8f70cedc5dbd52e8c2
analivv23 <ana.liviamoreira230310@gmail.com>
2025-09-03 01:07:14 +0000

    adicionamos um codigo de vetor

---
+++ vetor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int vetor[10];
+    int i, maior;
+
+   
+    printf("Digite 10 valores:\n");
+    for(i = 0; i < 10; i++) {
+        printf("Valor %d: ", i+1);
+        scanf("%d", &vetor[i]);
+    }
+
+   
+    maior = vetor[0]; 
+    for(i = 1; i < 10; i++) {
+        if(vetor[i] > maior) {
+            maior = vetor[i];
+        }
+    }
+
+    
+    printf("O maior valor digitado é: %d\n", maior);
+
+    return 0;
+}
